81ec616574872b763513b663042923927f520bbf
Leonardo Silva <leo.bs1997@hotmail.com>
2026-07-28 11:49:09 -0300

    feat: add Header componente and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
-"use client"
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 const Home = () => {
-  return <h1>home page</h1>
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Leonardo!</h2>
+        <p>28/07/2026</p>
+
+        <div className="mt-2 flex items-center gap-2">
+          <Input placeholder="Buscar" />
+
+          <Button className="rounded-lg bg-purple-600">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
