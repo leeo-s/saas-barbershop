5316bf6ce71a6cd106728f166554ead1d8e418f2
Leonardo Silva <leo.bs1997@hotmail.com>
2026-07-28 15:21:57 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,14 @@ import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import prisma from "@/config/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  // chamada do banco de dados
+  const barbershops = await prisma.barbershop.findMany({})
+  console.log(barbershops)
+
   return (
     <div>
       {/* header */}
@@ -36,7 +42,10 @@ const Home = () => {
         </div>
 
         {/* AGENDAMENTO */}
-        <Card className="mt-6">
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* ESQUERDA DO CARD */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -58,6 +67,16 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {/* esse [&::-webkit-scrollbar]:hidden vai esconder a barra de scroll que ficaria abaixo dos cards*/}
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
       </div>
     </div>
   )
--- config/prisma.ts
+++ config/prisma.ts
@@ -5,10 +5,27 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 })
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined
+// }
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+
+// globalForPrisma.prisma = prisma
+
+declare global {
+  // eslint-disable-next-line no-var -- "declare global" exige var, let/const não são aceitos aqui
+  var cachedPrisma: PrismaClient
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
+let prisma: PrismaClient
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient({ adapter })
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient({ adapter })
+  }
+  prisma = global.cachedPrisma
+}
 
-globalForPrisma.prisma = prisma
+export default prisma
