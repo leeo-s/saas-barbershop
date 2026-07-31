947bd26044b12a0280e50b54dc0676b4941302db
Leonardo Silva <leo.bs1997@hotmail.com>
2026-07-31 10:21:11 -0300

    chore: add next auth setup

--- app/_components/sidebar-sheet-content.tsx
+++ app/_components/sidebar-sheet-content.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image"
 import React from "react"
 import { Button } from "./ui/button"
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { quickSearchOptions } from "../_constants/QuickSearch"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 const SiderbarSheetContent = () => {
   return (
@@ -14,15 +21,36 @@ const SiderbarSheetContent = () => {
         <SheetTitle className="text-left">Menu</SheetTitle>
       </SheetHeader>
 
-      <div className="flex items-center gap-3 border-b border-solid py-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid py-5">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%] rounded-lg">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google.
+              </DialogDescription>
+            </DialogHeader>
+
+            <Button variant={"outline"} className="gap-1 font-bold">
+              <Image alt="Google" src={"/google.svg"} width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/* <Avatar>
           <AvatarImage src="https://utfs.io/f/0522fdaf-0357-4213-8f52-1d83c3dcb6cd-18e.png" />
         </Avatar>
 
         <div>
           <p className="font-bold">Leonardo Silva</p>
           <p className="text-xs">leonardo@email.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* OPÇÕES DE INÍCIO E AGENDAMENTOS */}
